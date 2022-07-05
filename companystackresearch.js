d5a872e83fa412044d6dba9814d58bb665de16eb
Liz Cosgro <ecosgro.6@gmail.com>
2022-07-05 12:11:19 -0400

    first commit

---
+++ companystackresearch.js
@@ -0,0 +1,7 @@
+    // extra credit
+    
+    
+    // Three software/technology companies that interest me are Adobe, Autodesk, and Apple.
+    // Adobe’s stack is known as the ARM Architecture Stack. The components to Adobe’s stack are OSGi Java Container, JCR Repository, and Sling. OSGi is used for modular development, and provides functionalities in the form of models, components, and services.The JCR Repository is the storage layer. It is where all code is stored and edited. Sling is the Web Application Framework, which is REST-based. It helps to consume and expose it to the JCR repository. 
+    // Autodesk’s stack includes Java, Apache HTTP Server, Bootstrap, jQuery UI, Amazon Cloudfront, Underscore, and more. Their top technologies used are Microsoft Azure, AWS CodePipeline, Hive AI, Tableau Software, MicroStrategy, and Azure Machine Learning . Autodesk holds a lot of key technologies and tools, but it was a bit difficult to find much more than that. 
+    // Apple’s stack includes Java, Spring Framework, Spring Boot, Hibernate, JUnit/TestNG, Swift, Objective-C, and more. Apple uses well over 1,000 different technologies.
---
+++ figma.js
@@ -0,0 +1,3 @@
+// figma design
+// link to figma part of assignment 
+// https://www.figma.com/file/7ENPM31fQMyRrWqtubLhq9/assessment-2-restaurant?node-id=0%3A1
